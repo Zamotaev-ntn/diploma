866142e3c0619a99a4c0a0f7bd39eba0cf3337d8
Zamotaev-ntn <antonioproskill@gmail.com>
2026-05-01 13:39:56 +0300

    постгрес подключен и регистрация работает

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash
+from config import Config
 from forms import RegisterForm
 from models import User
 from extensions import db
 from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
-app.secret_key = "secret"
+app.config.from_object(Config)
+
+db.init_app(app)
 
 
 @app.route("/")
@@ -16,21 +19,30 @@ def index():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegisterForm()
+
     if form.validate_on_submit():
         existing_user = User.query.filter_by(email=form.email.data).first()
+
         if existing_user:
             flash("Email уже зарегистрирован")
         else:
             user = User(
                 email=form.email.data,
-                password_hash=generate_password_hash(form.password.data),
+                password_hash=generate_password_hash(form.password.data)
             )
+
             db.session.add(user)
             db.session.commit()
+
             flash("Регистрация успешна")
+
         return redirect(url_for("register"))
+
     return render_template("register.html", form=form)
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True, host="127.0.0.1", port=5001)
