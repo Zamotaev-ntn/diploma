227ed907fff8350a437f5a0fc447ae219b5abe78
Zamotaev-ntn <antonioproskill@gmail.com>
2026-05-05 02:03:24 +0300

    Добавлен UserMixin и поле is_active в модель User

--- models.py
+++ models.py
@@ -1,15 +1,23 @@
 from extensions import db
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
     role = db.Column(db.Integer, default=1)
+    is_active = db.Column(db.Boolean, default=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
-        return f"<User {self.email}>"
+    @property
+    def password(self):
+        raise AttributeError("Password is not readable")
+
+    @password.setter
+    def password(self, value):
+        self.password_hash = generate_password_hash(value)
